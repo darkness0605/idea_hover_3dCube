0f273552dbe9fcdbc196c42e40714293562c48e1
darkness0605 <vnurlanov3@gmail.com>
2022-10-13 23:14:59 +0600

    add_animation

---
+++ script.js
@@ -0,0 +1,12 @@
+$('[data-path="1"]').hover(function(){ $('[data-path="1"]').css({"translate": "0 -50px"})})
+$('[data-path="1"]').mouseout(function(){ $('[data-path="1"]').css({"translate": "0 0px"})})
+
+$('[data-path="2"]').hover(function(){ $('[data-path="2"]').css({"translate": "0 -50px"})})
+$('[data-path="2"]').mouseout(function(){ $('[data-path="2"]').css({"translate": "0 0px"})})
+
+$('[data-path="3"]').hover(function(){ $('[data-path="3"]').css({"translate": "0 -50px"})})
+$('[data-path="3"]').mouseout(function(){ $('[data-path="3"]').css({"translate": "0 0px"})})
+
+$('[data-path="4"]').hover(function(){ $('[data-path="4"]').css({"translate": "0 -50px"})})
+$('[data-path="4"]').mouseout(function(){ $('[data-path="4"]').css({"translate": "0 0px"})})
+
